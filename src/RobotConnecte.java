7be76fa6742c3b062d488202b6c3d5cc08a60396
ZAKARIA <zakaria.lahrizi@insat.ucar.tn>
2025-05-02 11:25:20 +0100

    classe Robot Connecte

--- src/RobotConnecte.java
+++ src/RobotConnecte.java
@@ -1,8 +1,37 @@
 public abstract class RobotConnecte extends Robot implements Connectable{
     Boolean connecte;
-
+    String reseauConnecte;
     public RobotConnecte(String id, int x, int y, int energie) {
         super(id, x, y, energie);
+        this.connecte= false;
+        this.reseauConnecte= null;
     }
-
+    public void connecter(String reseau) {
+        if (this.connecte == false && this.verifierEnergie(5)) {
+            this.connecte = true;
+            this.reseauConnecte = reseau;
+            this.consommerEnergie(5);
+            this.ajouterHistorique("Robot connecté au réseau " + reseau);
+        } else {
+            this.ajouterHistorique("Robot déjà connecté au réseau " + this.reseauConnecte);
+        }
+    }
+    public void deconnecter() {
+        if (this.connecte == true) {
+            this.connecte = false;
+            this.reseauConnecte = null;
+            this.ajouterHistorique("Robot déconnecté");
+        } else {
+            this.ajouterHistorique("Robot déjà déconnecté");
+        }
+    }
+    public void envoyerDonnees(String donnees) {
+        if (this.connecte == true) {
+            this.consommerEnergie(3);
+            this.ajouterHistorique("Données envoyées : " + donnees);
+        } else {
+            this.ajouterHistorique("Robot non connecté, données non envoyées");
+        }
+    }
 }
+
